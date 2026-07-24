ecee8be71a006f1cfc30a00080378cc808705f91
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-07-24 20:58:27 +0700

    Add user endpoint into app.ts in the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import path from "path";
 import mahasiswaRoutes from "./routes/mahasiswa.route";
 import prodiRoutes from "./routes/prodi.route";
 import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/user.route";
 
 const app = express();
 
@@ -24,5 +25,7 @@ app.use("/api/prodi", prodiRoutes);
 app.use("/api/mahasiswa", mahasiswaRoutes);
 
 app.use("/api/auth", authRoutes);
+
+app.use("/api/user", userRoutes);
 
 export default app;
