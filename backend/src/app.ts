1f1f7111ab07fdaf1b43752c5766421b5dbb84c8
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-08 00:30:00 +0700

    Simplify CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,13 +8,7 @@ import userRoutes from "./routes/user.route";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.APP_URL || "http://localhost:3001",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 
