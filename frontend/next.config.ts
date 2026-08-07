e515321992742a26956ec04de6089faaa544e029
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-07 15:49:55 +0700

    Create NextConfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: "/sistem-akademik-mini",
+  assetPrefix: "/sistem-akademik-mini/",
 };
 
 export default nextConfig;
