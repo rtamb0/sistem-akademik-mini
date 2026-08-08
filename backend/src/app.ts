bdcc037ea8bb6dc079b06b0b9f2725def11dbbe9
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2026-08-08 08:49:37 +0700

    Add CORS configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,12 @@ import userRoutes from "./routes/user.route";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
